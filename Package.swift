fb1fd52082d51e4ac0dceca04df401e27b6fadb5
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2019-09-09 12:09:35 +0200

    update package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
@@ -18,7 +18,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "libsecp256k1",
+            name: "secp256k1",
             path: "./secp256k1/Classes",
             exclude: [
                 "secp256k1/build-aux",
@@ -57,10 +57,10 @@ let package = Package(
                 .headerSearchPath("secp256k1/src"),
                 .headerSearchPath(".")
             ]),
-        .target(
-            name: "secp256k1",
-            dependencies: ["libsecp256k1"],
-            path: "./secp256k1/Classes/exporter",
-            sources: ["."])
+//        .target(
+//            name: "secp256k1",
+//            dependencies: ["libsecp256k1"],
+//            path: "./secp256k1/Classes/exporter",
+//            sources: ["."])
     ]
 )
