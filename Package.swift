28d7d91b6d4ac04a5031d22811bda178c1ede006
Michele Balistreri <michele@bitgamma.com>
2024-09-18 11:03:53 +0200

    fixed SPM support

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "secp256k1",
-            targets: ["secp256k1"]),
+            targets: ["secp256k1Swift", "secp256k1C"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,9 +18,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "secp256k1",
+            name: "secp256k1C",
             path: "./secp256k1/Classes",
             exclude: [
+                "secp256k1.swift",
                 "secp256k1/build-aux",
                 "secp256k1/obj",
                 "secp256k1/sage",
@@ -56,6 +57,11 @@ let package = Package(
                 .headerSearchPath("secp256k1"),
                 .headerSearchPath("secp256k1/src"),
                 .headerSearchPath(".")
-            ])
+            ]),
+        .target(
+            name: "secp256k1Swift",
+            path: "./secp256k1/Classes",
+            sources: ["secp256k1.swift"]
+        )
     ]
 )
